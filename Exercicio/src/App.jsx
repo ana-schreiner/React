c73aa492ae5f8099b7ac059f6331a0c6fc0fb8c3
ana-schreiner <anaclaudiaschreiner@gmail.com>
2024-07-04 10:17:06 -0300

    Exercícios do dia 03/07/24

--- Exercicio/src/App.jsx
+++ Exercicio/src/App.jsx
@@ -5,13 +5,21 @@
 // Açucar sintático
 
 import Rodape from "./components/Rodape";
-import Blog from "./components/Blog";
+//import Blog from "./components/Blog";
+import CardAluno from "./components/CardAluno";
+import CardProduto from "./components/CardProduto";
 
 // Componente principal da aplicação
 function App() {
     return (
         <main>
-            <Blog />
+            <CardProduto nome="Liquidificador" precoUnitario={214.99} desconto={5} />
+            <CardProduto nome="Sanduicheira" precoUnitario={99.99} desconto={0} />
+            <CardProduto nome="Batedeira" precoUnitario={289.99} desconto={10} />
+            <CardAluno nome="Maria" serie="8º" media={8} />
+            <CardAluno nome="Ricardo" serie="8º" media={5} />
+            <CardAluno nome="Ângela" serie="8º" media={1} />
+            {/* <Blog /> */}
             <Rodape />
         </main>
     );
